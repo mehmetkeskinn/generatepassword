b0dbec7f9cd0703fee51ec3d9c8ac07b8cde174b
Mehmet Keskin <mehmetkeskin2696@gmail.com>
2023-02-23 22:19:23 -0500

    Initial commit

--- src/message.js
+++ src/message.js
@@ -1,2 +1,2 @@
-export const COPY_SUCCESS = "Password successfully copied to clipboard"
-export const COPY_Fail = "Password successfully copied to clipboard"
+export const COPY_SUCCESS = "Password successfully copied to clipboard";
+export const COPY_Fail = "Could not copy password";
